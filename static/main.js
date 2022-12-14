626b0560aaca0e112189670fe0c40e4ea66125ad
Kevin Stewart <94493923+kevin-stew@users.noreply.github.com>
2022-12-14 16:51:15 -0600

    first commit

---
+++ static/main.js
@@ -0,0 +1,128 @@
+// ---------------------------- FRONT SIDE ----------------------------
+
+document.getElementById('front-1').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("front-1-card").innerHTML = bar;
+});
+
+document.getElementById('front-2').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("front-2-card").innerHTML = bar;
+});
+
+document.getElementById('front-3').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("front-3-card").innerHTML = bar;
+});
+
+document.getElementById('front-4').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("front-4-card").innerHTML = bar;
+});
+
+document.getElementById('front-5').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("front-5-card").innerHTML = bar;
+});
+
+document.getElementById('front-6').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("front-6-card").innerHTML = bar;
+});
+
+document.getElementById('front-7').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("front-7-card").innerHTML = bar;
+});
+
+document.getElementById('front-8').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("front-8-card").innerHTML = bar;
+});
+
+document.getElementById('front-9').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("front-9-card").innerHTML = bar;
+});
+
+document.getElementById('front-10').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("front-10-card").innerHTML = bar;
+});
+
+document.getElementById('front-11').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("front-11-card").innerHTML = bar;
+});
+
+document.getElementById('front-12').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("front-12-card").innerHTML = bar;
+});
+
+
+// ---------------------------- BACK SIDE ----------------------------
+
+document.getElementById('back-1').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("back-1-card").innerHTML = bar;
+});
+
+document.getElementById('back-2').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("back-2-card").innerHTML = bar;
+});
+
+document.getElementById('back-3').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("back-3-card").innerHTML = bar;
+});
+
+document.getElementById('back-4').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("back-4-card").innerHTML = bar;
+});
+
+document.getElementById('back-5').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("back-5-card").innerHTML = bar;
+});
+
+document.getElementById('back-6').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("back-6-card").innerHTML = bar;
+});
+
+document.getElementById('back-7').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("back-7-card").innerHTML = bar;
+});
+
+document.getElementById('back-8').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("back-8-card").innerHTML = bar;
+});
+
+document.getElementById('back-9').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("back-9-card").innerHTML = bar;
+});
+
+document.getElementById('back-10').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("back-10-card").innerHTML = bar;
+});
+
+document.getElementById('back-11').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("back-11-card").innerHTML = bar;
+});
+
+document.getElementById('back-12').addEventListener('input', function() {
+    var bar = this.value
+    document.getElementById("back-12-card").innerHTML = bar;
+});
+
+function printDiv(divName) {
+    window.print();
+}
